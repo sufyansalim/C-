678103b5b7c595eb03a367c70616f43053bd310d
Sufyan Saleem <40366969+sufyansalim@users.noreply.github.com>
2020-09-03 02:54:07 +0500

    functions

---
+++ Functions/functions.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+// Prints the value of an integer and it's size, and increments the value
+int print_and_inc_int(int i) {
+  cout << "Value: " << i << ", S: " << sizeof(i) << endl;
+  return i + 1;
+}
+
+// Prints the value of a float and it's size, and increments the value
+float print_and_inc_float(float sp) {
+  cout << "Value: " << sp << ", S: " << sizeof(sp) << endl;
+  return sp + 1;
+}
+
+// Prints the value of a double and it's size, and increments the value
+double print_and_inc_double(double dp) {
+  cout << "Value: " << dp << ", S: " << sizeof(dp) << endl;
+  return dp + 1;
+}
+
+int main() {
+  // Declare and initialize variables
+  int i = 4362;
+  float sp = 20.123;
+  double dp = 194.421;
+
+  // Call our functions
+  // Return value overwrites our input argument (e.g. i becomes i + 1, returned
+  // from the function "print_and_inc_int(i)")
+  i = print_and_inc_int(i);
+  sp = print_and_inc_float(sp);
+  dp = print_and_inc_double(dp);
+
+  // Print out the returned values
+  cout << "New int value " << i << '\n';
+  cout << "New float value " << sp << '\n';
+  cout << "New double value " << dp << '\n';
+
+  return 0;
+
+}
---
+++ Functions/overloading.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+// Prints the value of an integer and it's size, and increments the value
+int print_and_inc(int i) {
+  cout << "Value: " << i << ", S: " << sizeof(i) << endl;
+  return i + 1;
+}
+
+// Prints the value of a float and it's size, and increments the value
+float print_and_inc(float sp) {
+  cout << "Value: " << sp << ", S: " << sizeof(sp) << endl;
+  return sp + 1;
+}
+
+// Prints the value of a double and it's size, and increments the value
+double print_and_inc(double dp) {
+  cout << "Value: " << dp << ", S: " << sizeof(dp) << endl;
+  return dp + 1;
+}
+
+int main() {
+  // Declare and initialize variables
+  int i = 4362;
+  float sp = 20.123;
+  double dp = 194.421;
+
+  // Call our overloaded functions
+  // Functions can be the same name as long as the signatures are different (the
+  // compiler can still tell them apart).
+  i = print_and_inc(i);
+  sp = print_and_inc(sp);
+  dp = print_and_inc(dp);
+
+  // Print out the returned values
+  cout << "New int value " << i << endl;
+  cout << "New float value " << sp << endl;
+  cout << "New double value " << dp << endl;
+
+  return 0;
+}
