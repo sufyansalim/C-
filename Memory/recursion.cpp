5639e7e0402fb5836bb4d193874dcce9bef7f316
Sufyan Saleem <40366969+sufyansalim@users.noreply.github.com>
2020-09-03 16:17:29 +0500

    Memory & Objects

---
+++ Memory/recursion.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cstdlib>
+
+/*////////////////////
+    PASS BY VALUE
+////////////////////*/
+
+
+using std::cout;
+
+// Our class from last example
+class Wallet {
+ private:
+  int dollars;
+  int cents;
+
+ public:
+  // Our constructors and copy constructor
+  Wallet(int d, int c);
+  Wallet() = default;
+  Wallet(const Wallet &w);
+
+  // Our methods
+  void print();
+  void set_dollars(int d) { dollars = d; }
+  void set_cents(int c) { cents = c; }
+  int get_dollars() const { return dollars; }
+  int get_cents() const { return cents; }
+};
+
+// Custom constructor
+Wallet::Wallet(int d, int c) {
+  dollars = d;
+  cents = c;
+}
+
+// Copy constructor
+Wallet::Wallet(const Wallet &w){
+  cout << "Calling the copy constructor!" << '\n';
+  dollars = w.get_dollars();
+  cents = w.get_cents();
+}
+
+void Wallet::print() {
+  cout << "Value of wallet = $" << dollars << "." << cents << '\n';
+}
+
+// Assigns a random number from 0-99 to the dollars and cents fields
+// Pass by value
+void randomWallet_pbv(Wallet w){
+  // The variable "w" is local to this function only!
+  // It's no longer valid when the function returns!
+  w.set_dollars(rand() % 100);
+  w.set_cents(rand() % 100);
+  cout << "Print from PBV function" << '\n';
+  w.print();
+}
+
+// Assigns a random number from 0-99 to the dollars and cents fields
+// Pass by reference
+void randomWallet_pbr(Wallet &w){
+  // The variable "w" now refers to the one from the main function!
+  // There is not copy constructor because we are modifying the original object
+  w.set_dollars(rand() % 100);
+  w.set_cents(rand() % 100);
+  cout << "Print from PBR function" << '\n';
+  w.print();
+}
+
+int main() {
+  // Let's start by creating a wallet
+  Wallet w1(10, 37);
+
+  // What happens when we pass a wallet to the function by value?
+  // The copy constructor gets called!
+  // A copy of the wallet is modified, not the original
+  randomWallet_pbv(w1);
+  
+  // Print out the values in main
+  cout << "Print from main" << '\n';
+  w1.print();
+
+  // When we pass a wallet by reference, we are not making a temporary!
+  // This is useful when we have large objects that would be expensive to copy.
+  randomWallet_pbr(w1);
+
+  // Print out the values in main
+  cout << "Print from main" << '\n';
+  w1.print();
+
+  return 0;
+}
---
+++ Objects/constructors.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using std::cout;
+
+// Our class from last example
+class Wallet {
+ private:
+  int dollars;
+  int cents;
+
+ public:
+  // Classes usually have constructors that say what to do when we make an
+  // object
+  // We'll define our own that initializes 'dollars' and 'cents' with some
+  // values
+  Wallet(int d, int c);
+
+  // We can also have a default constructor created by the compiler
+   Wallet() = default;
+
+  void print();
+
+  void set_dollars(int d) { dollars = d; }
+  void set_cents(int c) { cents = c; }
+  int get_dollars() { return dollars; }
+  int get_cents() { return cents; }
+};
+
+// Constructors have the same name as the class they belong to
+Wallet::Wallet(int d, int c){
+  dollars = d;
+  cents = c;
+}
+
+void Wallet::print() {
+  cout << "Value of wallet = $" << dollars << "." << cents << '\n';
+}
+
+int main() {
+  // Let's create one object using our constructor
+  Wallet w1(10, 37);
+  w1.print();
+
+  // If we don't write a default constructor, the compiler defines one for us
+  // Let's create one class using the default constructor
+  // Warning! This does not initialize the values!
+  Wallet w2;
+  // Likely does not print what we are expecting (garbage values)
+  w2.print();
+  
+  // Creating here we are zero-initialing a class, and assigning it to w3
+  Wallet w3 = Wallet();
+  //Wallet w3 = {};
+  w3.print();
+
+  return 0;
+}
